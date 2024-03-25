ded4c25a54e30f233d8e775c45458854e379dd42
Vedran Lajic <vedran.lajic237@gmail.com>
2024-03-25 22:00:16 +0000

    the app now works, along with some more additions|creating db

--- f1blog/settings.py
+++ f1blog/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-adbwx8j-5nqn#wjv^o=p#+#flkb(69@my^%^kz3jw$r4to#zc1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,9 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR)]
-STATIC_URL = '/staticfiles/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
