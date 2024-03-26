ee8977d074d3f36f78921a14f4a14d54d2207978
Vedran Lajic <vedran.lajic237@gmail.com>
2024-03-26 21:04:36 +0000

    trying if this works 1

--- f1blog/settings.py
+++ f1blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('django-insecure-adbwx8j-5nqn#wjv^o=p#+#flkb(69@my^%^kz3jw$r4to#zc1')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
