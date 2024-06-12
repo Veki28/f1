78fcfe1b08fda91b3960797390219974a7d33686
Vedran Lajic <vedran.lajic237@gmail.com>
2024-06-12 19:35:49 +0100

    Implemented editing and deleting comments (crud)

--- about/admin.py
+++ about/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import About
+from .models import About, CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
-
     summernote_fields = ('content',)
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+    list_display = ('message', 'read',)
--- about/views.py
+++ about/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
 # Create your views here.
+
+
 def about_me(request):
     """
     Renders the About page
     """
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS,
+                                 "Your collaboration request has been successfully received. The usual response time is around 3 working days.")
+
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about,
+         "collaborate_form": collaborate_form
+         },
     )
